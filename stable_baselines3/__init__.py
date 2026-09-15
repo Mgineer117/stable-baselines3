8e947c8513110deddea6f2237b033ff6ab225d04
Minjae Cho <whalswo117@gmail.com>
2026-09-15 17:26:39 -0500

    build: export IRPO from SB3 namespace

--- stable_baselines3/__init__.py
+++ stable_baselines3/__init__.py
@@ -5,6 +5,7 @@ from stable_baselines3.common.utils import get_system_info
 from stable_baselines3.ddpg import DDPG
 from stable_baselines3.dqn import DQN
 from stable_baselines3.her.her_replay_buffer import HerReplayBuffer
+from stable_baselines3.irpo import IRPO
 from stable_baselines3.ppo import PPO
 from stable_baselines3.sac import SAC
 from stable_baselines3.td3 import TD3
@@ -30,5 +31,6 @@ __all__ = [
     "SAC",
     "TD3",
     "HerReplayBuffer",
+    "IRPO",
     "get_system_info",
 ]
